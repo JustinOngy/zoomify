832267f8b05dc71dc894bbc6574d658c7da98220
Justin Ong <justin-ong@live.com.au>
2024-04-20 15:51:49 +1000

    first commit

---
+++ components/MobileNav.tsx
@@ -0,0 +1,79 @@
+"use client";
+import React from "react";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import Image from "next/image";
+import Link from "next/link";
+import { sidebarLinks } from "@/constants";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
+
+const MobileNav = () => {
+  const pathname = usePathname();
+
+  return (
+    <section className="w-full max-w-[264px]">
+      <Sheet>
+        <SheetTrigger>
+          <Image
+            src="/icons/hamburger.svg"
+            width={36}
+            height={36}
+            alt="hamburger"
+            className="cursor-pointer sm:hidden"
+          />
+        </SheetTrigger>
+        <SheetContent side="left" className="border-none bg-dark-1">
+          <Link href="/" className="flex items-center gap-2">
+            <Image
+              src="/icons/logo1.png"
+              width={32}
+              height={32}
+              alt="Zoomify logo"
+              className="max-sm:size-10"
+            />
+            <p className="text-[26px] font-extrabold text-white ">Zoomify</p>
+          </Link>
+          <div className="flex h-[calc(100vh - 72px)] flex-col justify-between overflow-y-auto">
+            <section className="flex h-full flex-col gap-6 pt-16 text-white">
+              {sidebarLinks.map((item) => {
+                const isActive = pathname === item.route;
+
+                return (
+                  <SheetClose asChild key={item.route}>
+                    <Link
+                      href={item.route}
+                      key={item.label}
+                      className={cn(
+                        "flex gap-4 items-center p-4 rounded-lg justify-start",
+                        {
+                          "bg-blue-1": isActive,
+                        }
+                      )}>
+                      <Image
+                        src={item.imgURL}
+                        alt={item.label}
+                        width={24}
+                        height={24}
+                      />
+                      <p className="text-lg font-semibold ">{item.label}</p>
+                    </Link>
+                  </SheetClose>
+                );
+              })}
+            </section>
+          </div>
+        </SheetContent>
+      </Sheet>
+    </section>
+  );
+};
+
+export default MobileNav;
